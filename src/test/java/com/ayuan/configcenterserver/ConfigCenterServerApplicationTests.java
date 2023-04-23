0565f604f3792fff5616cd2c99f27b30f9792049
Ayuan <838478249@qq.com>
2023-04-23 10:00:55 +0800

    feat:初始化项目

---
+++ src/test/java/com/ayuan/configcenterserver/ConfigCenterServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayuan.configcenterserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigCenterServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
